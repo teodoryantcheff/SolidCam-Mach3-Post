d19d432fbd25ea54dc3c5469918f5688359af367
Teodor Yantcheff <teodor.yantchev@gmail.com>
2016-03-08 22:35:58 +0200

    1st Y version. Also some samples.

---
+++ cp.cmd
@@ -0,0 +1,8 @@
+@echo off
+copy /y "c:\Users\Public\Documents\SolidCAM\SolidCAM2015\Gpptool\Mach3 - 4 axis X.gpp" "D:\SolidCam-Mach3-Post"
+copy /y "c:\Users\Public\Documents\SolidCAM\SolidCAM2015\Gpptool\Mach3 - 4 axis Y.gpp" "D:\SolidCam-Mach3-Post"
+copy /y "c:\Users\Public\Documents\SolidCAM\SolidCAM2015\Gpptool\Mach3_4X.vmid" "D:\SolidCam-Mach3-Post"
+copy /y "c:\Users\Public\Documents\SolidCAM\SolidCAM2015\Gpptool\Mach3_4Y.vmid" "D:\SolidCam-Mach3-Post"
+
+
+
